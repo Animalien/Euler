eb320bbadb6204079acedb19065b972dd88bc299
Animalien <Animalien@users.noreply.github.com>
2020-09-07 12:16:37 -0700

    Answered problem #8 - Largest product in series

--- ProjectEuler.cpp
+++ ProjectEuler.cpp
@@ -2,10 +2,12 @@
 
 #include <algorithm>
 #include <assert.h>
+#include <deque>
 #include <map>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string>
 #include <vector>
 
 
@@ -464,6 +466,84 @@ void RunNthPrime(BigInt n) {
 
 
 ////////////////////////////
+// Problem 8 - Largest product in series
+
+static const std::string s_largestProductInSeriesSeries =
+	"73167176531330624919225119674426574742355349194934"
+	"96983520312774506326239578318016984801869478851843"
+	"85861560789112949495459501737958331952853208805511"
+	"12540698747158523863050715693290963295227443043557"
+	"66896648950445244523161731856403098711121722383113"
+	"62229893423380308135336276614282806444486645238749"
+	"30358907296290491560440772390713810515859307960866"
+	"70172427121883998797908792274921901699720888093776"
+	"65727333001053367881220235421809751254540594752243"
+	"52584907711670556013604839586446706324415722155397"
+	"53697817977846174064955149290862569321978468622482"
+	"83972241375657056057490261407972968652414535100474"
+	"82166370484403199890008895243450658541227588666881"
+	"16427171479924442928230863465674813919123162824586"
+	"17866458359124566529476545682848912883142607690042"
+	"24219022671055626321111109370544217506941658960408"
+	"07198403850962455444362981230987879927244284909188"
+	"84580156166097919133875499200524063689912560717606"
+	"05886116467109405077541002256983155200055935729725"
+	"71636269561882670428252483600823257530420752963450";
+
+BigInt CalcLargestProductInSeries(BigInt numDigits) {
+	const BigInt seriesLength = s_largestProductInSeriesSeries.length();
+
+	std::deque<BigInt> factorDeque;
+
+	// now roll through the entire series, looking for the largest product of adjacent digits
+	// note: keep track of '0' digits separately because they zero-stomp a product,
+	//			and don't multiply them into the current product
+	BigInt largestProduct = 0;
+	BigInt currProduct = 1;
+	BigInt numZeroes = 0;
+	for (BigInt i = 0; i < seriesLength; ++i) {
+		// first get rid of the oldest factor (if we are at num digits)
+		if (i >= numDigits) {
+			const BigInt oldestFactor = factorDeque.front();
+			if (oldestFactor == 0) {
+				--numZeroes;
+			}
+			else {
+				currProduct /= oldestFactor;
+				//printf("%lld / %lld = %lld\n", currProduct * oldestFactor, oldestFactor, currProduct);
+			}
+			factorDeque.pop_front();
+		}
+
+		// now multiply in the new factor
+		const BigInt newFactor = (BigInt)(s_largestProductInSeriesSeries[i] - '0');
+		//printf("%c", s_largestProductInSeriesSeries[i]);
+
+		if (newFactor == 0) {
+			++numZeroes;
+		}
+		else {
+			currProduct *= newFactor;
+			//printf("%lld * %lld = %lld\n", currProduct / newFactor, newFactor, currProduct);
+		}
+		factorDeque.push_back(newFactor);
+
+		// finally compare to see if this is the new largest product
+		// (but only if we have enough digits and there are no zeroes in the sequence)
+		if ((i >= numDigits) && (numZeroes <= 0) && (currProduct > largestProduct)) {
+			largestProduct = currProduct;
+		}
+	}
+
+	return largestProduct;
+}
+
+void RunLargestProductInSeries(BigInt numDigits) {
+	printf("Largest product in 1000 digit series using %lld adjacent digits = %lld\n", numDigits, CalcLargestProductInSeries(numDigits));
+}
+
+
+////////////////////////////
 
 
 
@@ -522,6 +602,11 @@ int main(int argc, char** argv) {
 		RunNthPrime(6);
 		RunNthPrime(50);
 		RunNthPrime(10001);
+		break;
+	case 8:
+		RunLargestProductInSeries(4);
+		RunLargestProductInSeries(10);
+		RunLargestProductInSeries(13);
 		break;
 	default:
 		printf("'%s' is not a valid problem number!\n\n", problemArg);
